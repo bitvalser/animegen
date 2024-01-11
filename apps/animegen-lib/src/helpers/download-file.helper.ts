1e374f32f500b4ccc02d19f816ebccaeec3b9be6
Valery Bitkovsky <bitvalser@gmail.com>
2024-01-11 21:58:42 +0300

    fix donload file logic

--- apps/animegen-lib/src/helpers/download-file.helper.ts
+++ apps/animegen-lib/src/helpers/download-file.helper.ts
@@ -1,27 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 import { createWriteStream } from 'fs';
+
+const REQUEST_TIMEOUT = 60000;
 
 export async function downloadFile(fileUrl: string, outputLocationPath: string): Promise<void> {
   const writer = createWriteStream(outputLocationPath);
 
-  return axios
-    .get(fileUrl, {
-      responseType: 'stream',
-    })
-    .then((response: any) => {
-      return new Promise((resolve, reject) => {
-        response.data.pipe(writer);
-        let error: any = null;
-        writer.on('error', (err) => {
-          error = err;
-          writer.close();
-          reject(err);
-        });
-        writer.on('close', () => {
-          if (!error) {
-            resolve();
-          }
-        });
+  return new Promise((resolve, reject) => {
+    const abort = new AbortController();
+    const manualTimeout = setTimeout(() => {
+      abort.abort();
+      writer.destroy();
+    }, REQUEST_TIMEOUT);
+    axios
+      .get(fileUrl, {
+        responseType: 'stream',
+        signal: abort.signal,
+      })
+      .then(
+        (response: any) =>
+          new Promise<void>((innerResolve) => {
+            response.data.pipe(writer);
+            let error: any = null;
+            writer.on('error', (err) => {
+              error = err;
+              writer.close();
+              innerResolve();
+              reject(err);
+            });
+            writer.on('close', () => {
+              innerResolve();
+              if (!error) {
+                resolve();
+              }
+            });
+          }),
+      )
+      .catch(reject)
+      .finally(() => {
+        clearTimeout(manualTimeout);
       });
-    });
+  });
 }
